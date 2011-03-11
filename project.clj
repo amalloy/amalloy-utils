2be9d9cf12817a8f208d8256a0aa0ed09b21b0ec
Alan Malloy <alan@malloys.org>
2011-03-11 00:09:26 -0800

    Add macro-utils subpackage

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/utils "0.2.1"
+(defproject amalloy/utils "0.3.0"
   :description "TODO: add summary of your project"
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]])
--- src/amalloy/utils/macro.clj
+++ src/amalloy/utils/macro.clj
@@ -1,7 +1,19 @@
 (ns amalloy.utils.macro
   (:use clojure.contrib.macro-utils))
 
+(defn- partition-params [argvec actual-args]
+  (if (some #{'&} argvec)
+    [actual-args] ; one seq with all args
+    (partition (count argvec) actual-args)))
+
 (defmacro anon-macro
   ([args macro-body & body]
      `(macrolet [(name# ~args ~macro-body)]
                 (name# ~@body))))
+
+(defmacro macro-do
+  [macro-args body & args]
+  `(anon-macro [arg#]
+               (for [~macro-args arg#]
+                 ~body) ; (cons 'do) already supplied by macrolet
+               ~(partition-params macro-args args)))
