445cd75f355156f7a6f978750a218fb3883ee95c
Alan Malloy <alan@malloys.org>
2011-03-11 00:09:05 -0800

    Add a key-comparator

--- src/amalloy/utils/transform.clj
+++ src/amalloy/utils/transform.clj
@@ -1,4 +1,5 @@
-(ns amalloy.utils.transform)
+(ns amalloy.utils.transform
+  (:require [amalloy.utils :as core]))
 
 (defn transform-if
   "Returns a function that tests pred against its argument. If the result
@@ -15,3 +16,22 @@ identity)."
 (def make-str (transform-if #(instance? clojure.lang.Named %) name str))
 (def make-kw keyword)
 (def make-int (transform-if string? #(Integer/parseInt %)))
+
+(defn key-comparator
+  "Given a transformation function (and optionally a direction), return a comparator which does its work by comparing the values of (transform x) and (transform y)."
+  ([modifier]
+     (fn [a b]
+       (- (modifier a) (modifier b))))
+  ([direction modifier]
+     (let [f (comparator modifier)]
+       (condp core/invoke direction
+         #{:desc :descending -} (comp - f)
+         #{:asc :ascending +} f))))
+
+
+
+
+
+
+
+
