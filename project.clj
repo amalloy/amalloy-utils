dd0b0470adfc7864bc3c86d3d71b5783ceebea26
Alan Malloy <alan@malloys.org>
2011-03-15 10:13:30 -0700

    Cut a new release with hot-potato

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/utils "0.3.0"
-  :description "TODO: add summary of your project"
+(defproject amalloy/utils "0.3.1"
+  :description "A collection of handy-dandy tools I've collected as I notice repeated patterns in my Clojure code"
   :dependencies [[clojure "1.2.0"]
                  [clojure-contrib "1.2.0"]])
