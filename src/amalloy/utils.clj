7aa911b03898fa6c12fc56f98ee7e32eef253e43
Alan Malloy <alan@malloys.org>
2011-03-30 12:51:47 -0700

    Add ffilter

--- src/amalloy/utils.clj
+++ src/amalloy/utils.clj
@@ -24,6 +24,13 @@
   [f start]
   trim-seq rest iterate)
 
+(defcomp ffilter
+  "Like clojure.core/some, but instead of returning (pred x) for the
+  matching element x, it returns x. Useful for predicates that return
+  true/false instead of their argument."
+  [pred coll]
+  first filter)
+
 (defn decorate
   "Return a function f such that (f x) => [x (f1 x) (f2 x) ...]."
   [& fs]
