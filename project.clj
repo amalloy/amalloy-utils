1c71ec899c3722fd46e07716ceb590097743f5ac
Alan Malloy <alan@malloys.org>
2011-04-01 11:06:32 -0700

    Don't demand clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject amalloy/utils "0.3.4"
   :description "A collection of handy-dandy tools I've collected as I notice repeated patterns in my Clojure code"
-  :dependencies [[clojure "1.2.1"]
-                 [clojure-contrib "1.2.0"]]
+  :dependencies [[clojure "[1.2.0,)"]
+                 [clojure-contrib "[1.2.0,)"]]
   :dev-dependencies [[org.clojars.gjahad/swank-clojure "1.3.1.1-SNAPSHOT"]
                      [clojure-source "1.2.1"]
                      [clj-stacktrace "0.2.0"]]
